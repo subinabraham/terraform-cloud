df70250f3cbf35aa2500a7696cc79dc59f549839
subinabraham <subin2190@gmail.com>
2026-02-26 10:35:48 +0000

    add dev

---
+++ dev/main.tf
@@ -0,0 +1,46 @@
+
+############################################################
+# BigQuery Dataset
+############################################################
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id                  = var.dataset_id
+  location                    = "US"
+  description                 = "Example dataset created with Terraform"
+  delete_contents_on_destroy  = true
+
+  labels = {
+    environment = "dev"
+    owner       = "terraform"
+  }
+}
+
+############################################################
+# BigQuery Table
+############################################################
+resource "google_bigquery_table" "table" {
+  dataset_id = google_bigquery_dataset.dataset.dataset_id
+  table_id   = var.table_id
+
+  schema = jsonencode([
+    {
+      name = "id"
+      type = "STRING"
+      mode = "REQUIRED"
+    },
+    {
+      name = "timestamp"
+      type = "TIMESTAMP"
+      mode = "REQUIRED"
+    },
+    {
+      name = "value"
+      type = "FLOAT"
+      mode = "NULLABLE"
+    }
+  ])
+
+  labels = {
+    environment = "dev"
+    owner       = "terraform"
+  }
+}
---
+++ dev/variables.tf
@@ -0,0 +1,26 @@
+############################################################
+# Variables
+############################################################
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "The default region for resources"
+  type        = string
+  default     = "US"
+}
+
+variable "dataset_id" {
+  description = "BigQuery dataset ID"
+  type        = string
+  default     = "my_dataset"
+}
+
+variable "table_id" {
+  description = "BigQuery table ID"
+  type        = string
+  default     = "my_table"
+}
+ 

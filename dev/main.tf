f4737567fb8ece99908334500f31e7cff46e1042
subinabraham <subin2190@gmail.com>
2026-03-02 16:01:59 +0000

    testing

--- dev/main.tf
+++ dev/main.tf
@@ -10,8 +10,9 @@ resource "google_bigquery_dataset" "dataset" {
 
   labels = {
     environment = "dev"
-    owner       = "subin"
+    owner       = "subinabraham"
     org         = "coned"
+    
 
 
   }
